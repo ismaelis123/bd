ae979fed187eebaf812fa59b9260d46029b1c098
romeroismael965@gmail.com <rmNHHBMn8V9@r@5>
2026-03-25 15:28:37 -0600

    cambios nuevos login

--- src/database/supabaseconfig.js
+++ src/database/supabaseconfig.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
+const supabaseUrl = "https://rgpjhseivhxzictjqgch.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJncGpoc2Vpdmh4emljdGpxZ2NoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNTQxMDgsImV4cCI6MjA4ODgzMDEwOH0.7W5NN-ZVDbTN5cnuAEwt-q8sqxOgJStAoo39Vix1aA0";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
--- src/login/FormularioLogin.jsx
+++ src/login/FormularioLogin.jsx
@@ -1,4 +1,47 @@
-function FormularioLogin() {
-  return <div>Formulario Login</div>;
-}
+import { useState } from "react";
+import { supabase } from "../database/supabaseconfig";
+
+const FormularioLogin = ({ setSesion }) => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const iniciarSesion = async (e) => {
+    e.preventDefault();
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    });
+
+    if (error) {
+      alert("Error al iniciar sesión");
+    } else {
+      setSesion(data.user);
+    }
+  };
+
+  return (
+    <form onSubmit={iniciarSesion} className="card">
+      <h2>Iniciar Sesión</h2>
+
+      <input
+        type="email"
+        placeholder="Correo"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      /><br /><br />
+
+      <input
+        type="password"
+        placeholder="Contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      /><br /><br />
+
+      <button className="btn">Ingresar</button>
+    </form>
+  );
+};
+
 export default FormularioLogin;
